05089b22575014ae9af8d1e5b5fe2d33dbc55382
Samuel Birch <sam.birch@gmail.com>
2010-06-28 09:52:38 +0100

    Added Browser.Plugins.Silverlight to Xappa

--- Source/Utilities/Xappa.js
+++ Source/Utilities/Xappa.js
@@ -3,7 +3,7 @@
 
 name: Xappa
 
-description: Wrapper for embedding Silverlight movies.
+description: Wrapper for embedding Silverlight movies & Silverlight detection.
 
 license: MIT-style license.
 
@@ -12,10 +12,32 @@ credits:
 
 requires: [Options, Object]
 
-provides: Xappa
+provides: [Browser.Plugins.Silverlight, Xappa]
 
 ...
 */
+
+
+Browser.Plugins.Silverlight = (function(){
+	
+	var version;
+	
+	if(Browser.Engine.trident){
+		var agc = new ActiveXObject('AgControl.AgControl');
+		if(agc.IsVersionSupported('1.0')){version = 1;}
+		if(agc.IsVersionSupported('2.0')){version = 2;}
+		if(agc.IsVersionSupported('3.0')){version = 3;}
+		if(agc.IsVersionSupported('4.0')){version = 4;}
+	}else{
+		version = navigator.plugins['Silverlight Plug-In'].description.substring(0,1);
+	}
+	
+	if(!version){
+		version = 0;
+	}
+	
+	return {version: Number(version) || 0};
+})();
 
 
 var Xappa = new Class({
